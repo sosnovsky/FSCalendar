f99603f6f7011b4191fdb0b6760864ad6af21a1e
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2019-04-10 15:37:55 +0300

    Update FSCalendarStickyHeader.m

--- FSCalendar/FSCalendarStickyHeader.m
+++ FSCalendar/FSCalendarStickyHeader.m
@@ -43,7 +43,7 @@
         self.titleLabel = label;
         
         view = [[UIView alloc] initWithFrame:CGRectZero];
-        view.backgroundColor = FSCalendarStandardLineColor;
+        view.backgroundColor = [UIColor clearColor];
         [_contentView addSubview:view];
         self.bottomBorder = view;
         
